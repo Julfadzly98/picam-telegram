978d64c9e313e8a1cb7a9bdf0ca1319e04d25cc9
julfadzlymuhaimin98 <57395950+Julfadzly98@users.noreply.github.com>
2023-10-23 00:19:20 +0800

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,45 +1,49 @@
 #pip install picamera python-telegram-bot
 
+from urllib import request
 import time
 import picamera
 import requests
 import os
+from time import sleep
 
 from telegram import Bot
 
-# Replace with your Telegram Bot API token
+# Replace with your Telegram Bot API token and chat ID
 TELEGRAM_API_TOKEN = '5642615236:AAG6x-p2z1Q-nj9anic2N3GTM_gDR3D7Pno'
-# Replace with your chat ID
 TELEGRAM_CHAT_ID = '187740907'
 
 #'5642615236:AAG6x-p2z1Q-nj9anic2N3GTM_gDR3D7Pno'
 #'187740907'
 
-# Initialize the Telegram Bot
 bot = Bot(token=TELEGRAM_API_TOKEN)
 
-# Create a function to capture and send the image
 def capture_and_send_image():
+    success = False
+    
     try:
-        # Initialize the Pi Camera
         with picamera.PiCamera() as camera:
-            # Capture an image
             image_path = '/path/to/your/image.jpg'
             camera.capture(image_path)
             
-            # Send the image to Telegram
             with open(image_path, 'rb') as image_file:
                 bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=image_file)
                 
-            # Remove the captured image
             os.remove(image_path)
             
-            print("Image sent successfully.")
+            success = True
             
     except Exception as e:
         print("Error:", e)
+    
+    return success
 
-# Main loop
 while True:
-    capture_and_send_image()
-    time.sleep(10)  # Capture and send an image every 10 seconds
+    success = capture_and_send_image()
+    if success:
+        result = "Yes"
+    else:
+        result = "No"
+    print(f"Image sent successfully? {result}")
+    time.sleep(10)
+
